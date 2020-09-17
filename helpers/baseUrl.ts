c0c03ffde29823ba4f0bec1c14e7ccb5836284be
hadasSerussi <46957457+hadasSerussi@users.noreply.github.com>
2020-09-17 10:30:41 +0300

    update

--- helpers/baseUrl.ts
+++ helpers/baseUrl.ts
@@ -1,2 +1,3 @@
-const baseUrl = process.env.NODE_ENV === 'production' ? "https://ls-todos-task.vercel.app": "http://localhost:3000"
+// const baseUrl = process.env.NODE_ENV === 'production' ? "https://ls-todos-task-fp5wvd5yx.vercel.app": "http://localhost:3000"
+const baseUrl ="http://localhost:3000"
 export default baseUrl
